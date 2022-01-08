96bc762ccd102f47bccb2b0ca6e0f16327699f1a
Maurice Yang <mauriceyangmy28@gmail.com>
2022-01-08 20:59:05 +0700

    Changed some meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/App.css'
 import Head from 'next/head'
+import Logo from '../public/logo.png'
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -10,16 +11,16 @@ function MyApp({ Component, pageProps }) {
             <meta name="description" content="We are Family Mini Store. Shop your daily needs with us! We sell biscuits,  instant coffees, teas, sweets, dairy products, snacks, and new year products! The quality of our products are remarkable!"/>
 
             <meta property="og:type" content="website"/>
-            <meta property="og:url" content="https://metatags.io/"/>
+            <meta property="og:url" content="https://family-mini-store.netlify.app/"/>
             <meta property="og:title" content="Family Mini Store | Shop your daily needs with us!"/>
             <meta property="og:description" content="We are Family Mini Store. Shop your daily needs with us! We sell biscuits,  instant coffees, teas, sweets, dairy products, snacks, and new year products! The quality of our products are remarkable!"/>
-            <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+            <meta property="og:image" content={Logo}/>
 
             <meta property="twitter:card" content="summary_large_image"/>
-            <meta property="twitter:url" content="https://metatags.io/"/>
+            <meta property="twitter:url" content="https://family-mini-store.netlify.app/"/>
             <meta property="twitter:title" content="Family Mini Store | Shop your daily needs with us!"/>
             <meta property="twitter:description" content="We are Family Mini Store. Shop your daily needs with us! We sell biscuits,  instant coffees, teas, sweets, dairy products, snacks, and new year products! The quality of our products are remarkable!"/>
-            <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+            <meta property="twitter:image" content={Logo}/>
         </Head>
         <Component {...pageProps }/>
         </>
