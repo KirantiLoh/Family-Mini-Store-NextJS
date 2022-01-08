b3a197f54e70539c5a3b792d55b3c8ee91581381
Maurice Yang <mauriceyangmy28@gmail.com>
2022-01-08 20:10:16 +0700

    Installed sharp and added formats in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     reactStrictMode: true,
     images: {
         domains: ['res.cloudinary.com'],
-        formats: ['images/png', 'images/jpeg', 'images/jpg'],
+        formats: ['images/*'],
     }
 }
